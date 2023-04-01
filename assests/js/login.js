749ced38b85c276cafd3cf185dd82cf55be5f802
Ron55k2 <guninder55k2@gmail.com>
2023-04-01 07:59:01 +0530

    login page complete

---
+++ assests/js/login.js
@@ -0,0 +1,9 @@
+function login(){
+    
+    var data = {
+        email : document.getElementById("email").value,
+        password : document.getElementById("pass").value   
+    }
+
+    console.log(data);
+}
